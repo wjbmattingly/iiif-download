1162fd2530a21be7a50313cfc860edf3b72e5139
Segolene-Albouy <segolene.albouy@gmail.com>
2025-04-14 14:02:29 +0200

    [IMG] custom image name

--- src/iiif_download/__init__.py
+++ src/iiif_download/__init__.py
@@ -9,6 +9,6 @@ from .config import Config, config
 from .image import IIIFImage
 from .manifest import IIIFManifest
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 __all__ = ["IIIFManifest", "IIIFImage", "config", "Config"]
